21782f6157e09f41dc783211f451a3ec4207593b
shawn <sgiovanni1@yahoo.com>
2024-06-28 17:23:34 -0400

    binary search and linerar search

--- 2-MOD/practice-linearSearch/binarySearch.js
+++ 2-MOD/practice-linearSearch/binarySearch.js
@@ -3,12 +3,19 @@ function binarySearch(arr,target){
     let end = arr.length-1;
     let mid =Math.floor ((start + end) / 2)
 
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-
-
+    while(arr[mid] !== target && start <= end){
+        if(target < arr[mid]) end = mid + 1
+        else{
+            start = mid + 1
+        }
+        mid =Math.floor ((start + end) / 2)
     }
+    if(arr[mid] === target){
+        return mid;
+    }
+    return -1
 }
 
 console.log(binarySearch([2,4,6,8], 6));  // 2
 console.log(binarySearch([2,4,6,8], 10));  // -1
+
