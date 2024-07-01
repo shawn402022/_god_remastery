25a6c6a49b9b7006a82fd1558a7503405cfa8236
Shawn <sgiovanni1@yahoo.com>
2024-07-01 10:12:37 -0400

    learned graphs traversal

--- 2-MOD/aa06-binary-search-tree/tree-practice.js
+++ 2-MOD/aa06-binary-search-tree/tree-practice.js
@@ -25,51 +25,159 @@ function findMaxBST (rootNode) {
 }
 
 function findMinBT (rootNode) {
-  // Your code here
+
+  let min = rootNode.val
+  if(rootNode.left) {
+    min = Math.min(min, findMinBT(rootNode.left));
+  }
+
+  if(rootNode.right) {
+    min = Math.min(min, findMinBT(rootNode.right));
+  }
+
+  return min;
 }
 
 function findMaxBT (rootNode) {
-  // Your code here
+
+  let max = rootNode.val
+  if(rootNode.left) {
+    max = Math.max(max, findMaxBT(rootNode.left));
+  }
+
+  if(rootNode.right) {
+    max = Math.max(max, findMaxBT(rootNode.right ));
+  }
+
+  return max
 }
 
 function getHeight (rootNode) {
   // Your code here
+  if(!rootNode) return -1;
+  if(!rootNode.left && !rootNode.right) return 0;
+
+  return 1 + Math.max(getHeight(rootNode.left), getHeight(rootNode.right))
 }
 
 function balancedTree (rootNode) {
-  // Your code here
+  let queue = [rootNode];
+
+  while(queue.length){
+    let curr = queue.shift();
+
+    if(Math.abs(getHeight(curr.left) - getHeight(curr.right)) <= 1){
+      if(curr.left) queue.push(curr.left);
+      if(curr.right) queue.push(curr.right);
+    } else {
+      return false;
+    }
+
+  }
+  return true;
 }
 
 function countNodes (rootNode) {
-  // Your code here
+  if(!rootNode) return 0;
+
+  return 1 + countNodes(rootNode.left) + countNodes(rootNode.right);
 }
 
 function getParentNode (rootNode, target) {
-  // Your code here
+  if (rootNode.val === target) return null;
+
+  let stack = [rootNode];
+
+  while(stack.length) {
+    let curr = stack.pop();
+
+    if((curr.left && curr.left.val === target) || (curr.right && curr.right.val === target)) {
+      return curr;
+    }
+
+    if(curr.left) stack.push(curr.left);
+    if(curr.right) stack.push(curr.right);
+  }
+  return undefined;
 }
 
 function inOrderPredecessor (rootNode, target) {
-  // Your code here
+  let curr = rootNode;
+  let stack = [];
+  let pre = null;
+
+  while (true) {
+    if(curr) {
+      stack.push(curr);
+      curr = curr.left;
+    } else if (!curr && stack.length > 0){
+      curr = stack.pop();
+
+      if(curr.val === target) {
+        if(!pre) return null;
+        return pre.val;
+      }
+
+      pre = curr;
+      curr = curr.right;
+    } else {
+      break;
+    }
+  }
 }
 
 function deleteNodeBST(rootNode, target) {
   // Do a traversal to find the node. Keep track of the parent
-
+  let parentNode = getParentNode(rootNode, target)
   // Undefined if the target cannot be found
+  if(parentNode === undefined) return undefined;
 
   // Set target based on parent
+  let targetNode;
+  let isLeftChild = false;
+  if(!parentNode) {
+    targetNode = rootNode;
+  } else if(parentNode.left && parentNode.left.val === target){
+    targetNode = parentNode.left;
+    isLeftChild = true;
+  } else if(parentNode.right && parentNode.right.val === target) {
+    targetNode = parentNode.right;
+  } else {
+    throw Error ('Wrong');
+  }
+    // Case 0: Zero children and no parent:
+    //   return null
+  if(!parentNode && !targetNode.left && !targetNode.right) {
+    return null;
+  } else if ( !target.Node.left && !targetNode.right) {
+    // Case 1: Zero children:
+    //   Set the parent that points to it to null
+    if(isLeftChild) parentNode.left = null;
+    else parentNode.right = null;
+  } else if(targetNode.left && targetNode.right) {
+    // Case 2: Two children:
+    //  Set the value to its in-order predecessor, then delete the predecessor
+    //  Replace target node with the left most child on its right side,
+    //  or the right most child on its left side.
+    //  Then delete the child that it was replaced with.
 
-  // Case 0: Zero children and no parent:
-  //   return null
+    let pre = inOrderPredecessor(rootNode, target);
+    deleteNodeBST(rootNode, pre);
+    targetNode.val = pre;
+  } else {
+    if(targetNode.left) {
+      if(isLeftChild) parentNode.left = targetNode.left;
+      else parentNode.right = targetNode.left;
+    } else {
+      if (isLeftChild) parentNode.left = targetNode.left;
+      else parentNode.right = targetNode.right
+    }
+  }
 
-  // Case 1: Zero children:
-  //   Set the parent that points to it to null
 
-  // Case 2: Two children:
-  //  Set the value to its in-order predecessor, then delete the predecessor
-  //  Replace target node with the left most child on its right side,
-  //  or the right most child on its left side.
-  //  Then delete the child that it was replaced with.
+
+
+
 
   // Case 3: One child:
   //   Make the parent point to the child
---
+++ 2-MOD/aa06-breadth-first-search/problems/01-breadth-first-search.js
@@ -0,0 +1,44 @@
+/*
+Write a function called breadthFirstSearch that takes two nodes as arguments and
+will traverse the given graph breadth-first, returning true if there is a path
+from the first node to the second, and false if there is not.
+
+For this exercise, try to write your code from scratch. It's good practice!
+*/
+
+const adjList = {
+    1: [2, 5],
+    2: [1, 3, 5],
+    3: [2, 4],
+    4: [3, 5],
+    5: [1, 2, 4],
+    6: []
+}
+
+function breadthFirstSearch(start, end) {
+  // Your code here
+  const queue = [start];
+  const visited = new Set();
+
+  while(queue.length) {
+    let node = queue.shift();
+
+    if(!visited.has(node)) {
+      visited.add(node);
+
+      if(node === end) return true;
+      for(let neighbor of adjList[node]) {
+        queue.push(neighbor);
+      }
+    }
+  }
+  return false
+}
+
+// console.log(breadthFirstSearch(1, 3)); // -> true
+// console.log(breadthFirstSearch(4, 1)); // -> true
+// console.log(breadthFirstSearch(6, 1)); // -> false
+
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+module.exports = breadthFirstSearch;
