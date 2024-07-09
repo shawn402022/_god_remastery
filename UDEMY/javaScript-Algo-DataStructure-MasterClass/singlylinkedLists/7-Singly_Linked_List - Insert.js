0fd8d71b671ad75b850bf0240519778e499dacf9
Shawn <sgiovanni1@yahoo.com>
2024-07-09 18:39:05 -0400

    insert for singlylinked

--- UDEMY/javaScript-Algo-DataStructure-MasterClass/singlylinkedLists/7-Singly_Linked_List - Insert.js
+++ UDEMY/javaScript-Algo-DataStructure-MasterClass/singlylinkedLists/7-Singly_Linked_List - Insert.js
@@ -95,17 +95,10 @@ class SinglyLinkedList{
         return false;
     }
     insert(index, val){
-        if(index < 0 || index > this.length) return false;
-        if(index === this.length) return !!this.push(val);
-        if(index === 0) return !!this.unshift(val);
+        //const newNode = new Node(val);
 
-        var newNode = new Node(val);
-        var prev = this.get(index - 1);
-        var temp = prev.next;
-        prev.next = newNode;
-        newNode.next = temp;
-        this.length++;
-        return true;
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) return
     }
 }
 
